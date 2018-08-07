7809f618ebdc442903a9719c7045d4faa1a1157a
Ted Capco <tedcapco@gmail.com>
2018-08-07 23:25:03 +0800

    update changes

--- app.js
+++ app.js
@@ -31,15 +31,7 @@ client.connect()
 		console.log('Error');
 	})
 
-/*
-INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Double Monk Strap - Black', 'Footware', 'Originally, The monk strap had a difficult time being placed in either the formal or casual ends of the shoe-wearing spectrum. Considered too casual to be worn with a suit and too formal for many casual environments.', 'Marquins', 2195.00, '/Double Monk Strap Black.jpg');
-INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Longwing - Cognac Brown', 'Footware', 'Greenwich Vintage’s built-to-order cognac brown longwing with white mid-sole and baby blue commando sole.', 'Marquins', 2395.00, '/Longwing Cognac Brown.jpg');
-INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Captoe - Black', 'Footware', 'In the recent past, we covered various types of men’s boots and shoes, both formal and casual. In our Brogues Shoe Guide for Men, we touched lightly upon the difference between Oxford and Derby shoes.', 'Marquins', 2195.00, '/Captoe Black.jpg');
-INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Captoe - Cognac Brown', 'Footware', 'Cap toe Italian Leather dress shoes in contrasting brown leathers. The Cap Toe Griffin Brown Antique is crafted from smooth leather with a cap toe.', 'Marquins', 2195.00, '/Captoe Cognac Brown.jpg');
-INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Brogue Captoe Oak Brown', 'Footware', 'The Half Brogue Kendrik cap toe dress shoes in brown antique is hand-made from premium Italian leather with a cap toe and bold color combination.', 'Marquins', 2195.00, '/Brogue Captoe Oak Brown.jpg');
-INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Longwing - Black', 'Footware', 'Black French Calf upper framed with matching black edge stain on our Leo last. Historically, a pattern color combination that is recognized as the "work horse" classic business shoe.', 'Marquins', 2395.00, '/Longwing Black.jpg');
 
-*/
 //View engine setup
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
@@ -78,6 +70,159 @@ app.get('/products/:id', (req,res)=>{
 	});
 });
 
+
+app.get('/', function(req,res) { //product list
+	client.query('SELECT * FROM products ORDER BY products.id', (req, data)=>{
+		var list = [];
+		for (var i = 0; i < data.rows.length; i++) {
+			list.push(data.rows[i]);
+		}
+		res.render('home',{
+			data: list,
+			title: 'Product List'
+		});
+	});
+});
+
+app.get('/product/create', (req,res)=>{	//CREATE PRODUCT html
+	client.query('SELECT * FROM products_category', (req, data)=>{
+		var list = [];
+		for (var i = 1; i < data.rows.length+1; i++) {
+				list.push(data.rows[i-1]);
+		}
+		client.query('SELECT * FROM brands', (req, data)=>{
+			var list2 = [];
+			for (var i = 1; i < data.rows.length+1; i++) {
+					list2.push(data.rows[i-1]);
+			}
+			res.render('product_create',{
+				data: list,
+				data2: list2
+			});
+		});
+	});
+});
+
+
+app.post('/', function(req,res) { //product list with insert new product
+	var values =[];
+	values = [req.body.product_name,req.body.product_description,req.body.tagline,req.body.price,req.body.warranty,req.body.pic,req.body.category_id,req.body.brand_id];
+	//console.log(req.body);
+	//console.log(values);
+	client.query("INSERT INTO products(product_name, product_description, tagline, price, warranty, pic, category_id, brand_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8)", values, (err, res)=>{
+		if (err) {
+			console.log(err.stack)
+			}
+		else {
+			console.log(res.rows[0])
+		}
+	});
+	res.redirect('/');
+	});
+
+
+app.post('/brands', function(req,res) { //brand list insert 
+	var values =[];
+	values = [req.body.brand_name,req.body.brand_description];
+	console.log(req.body);
+	console.log(values);
+	client.query("INSERT INTO brands(brand_name, brand_description) VALUES($1, $2)", values, (err, res)=>{
+		if (err) {
+			console.log(err.stack)
+			}
+		else {
+			console.log(res.rows[0])
+		}
+	});
+	res.redirect('/brands');
+});
+
+
+
+app.get('/brands', (req,res)=>{ //brand list
+	client.query('SELECT * FROM brands', (req, data)=>{
+		var list = [];
+		for (var i = 1; i < data.rows.length+1; i++) {
+				list.push(data.rows[i-1]);
+		}
+		res.render('brands',{
+			data: list
+		});
+	});
+});
+
+app.get('/brand/create', (req,res)=>{ //route to create brand html
+	res.render('brand_create');
+});
+
+
+
+app.post('/categories', function(req,res){ //category list with insert new category query
+	var values =[];
+	values = [req.body.category_name];
+	console.log(req.body);
+	console.log(values);
+	client.query("INSERT INTO products_category(category_name) VALUES($1)", values, (err, res)=>{
+		if (err) {
+			console.log(err.stack)
+			}
+		else {
+			console.log(res.rows[0])
+		}
+	});
+	res.redirect('/categories');
+});
+
+
+app.get('/categories', (req,res)=>{ //category list
+	client.query('SELECT * FROM products_category', (req, data)=>{
+		var list = [];
+		for (var i = 1; i < data.rows.length+1; i++) {
+				list.push(data.rows[i-1]);
+		}
+		res.render('categories',{
+			data: list
+		});
+	});
+});
+
+app.get('/category/create', (req,res)=>{ //route to create category
+	res.render('categories_create');
+});
+
+
+
+app.get('/product/update/:id', (req,res)=>{
+	var id = req.params.id;
+	client.query('SELECT products.id, products.product_name, products.product_description, products.tagline, products.price, products.warranty, products.pic, products.category_id, products_category.category_name, products.brand_id, brands.brand_name FROM products INNER JOIN products_category ON products.category_id = products_category.id INNER JOIN brands ON products.brand_id = brands.id ORDER BY products.id' , (req, data)=>{
+		var list = [];
+		//console.log(data);
+		for (var i = 1; i < data.rows.length+1; i++) {
+			if (i==id) {
+				list.push(data.rows[i-1]);
+			}
+		}
+		//console.log(list);
+			client.query('SELECT * FROM products_category', (req, data)=>{
+			var list2 = [];
+			for (var i = 1; i < data.rows.length+1; i++) {
+				list2.push(data.rows[i-1]);
+			}
+			client.query('SELECT * FROM brands', (req, data)=>{
+				var list3 = [];
+				for (var i = 1; i < data.rows.length+1; i++) {
+					list3.push(data.rows[i-1]);
+				}
+				res.render('product_update',{
+					products: list,
+					products_category: list2,
+					brands: list3
+				});
+			});
+		});
+	});
+});
+
 app.get('/edit', (req,res)=>{
 	var id = req.params.id;
 	res.render('edit');
@@ -100,22 +245,18 @@ app.post('/products/:id/send', function(req, res) {
 
 	//nodemailer
 	let transporter = nodemailer.createTransport({
-        host: 'smtp.mail.yahoo.com',
+        host: 'smtp.mail.gmail.com',
         port: 465,
         secure: true,
         auth: {
-            user: 'iemaniamailer@yahoo.com', 
-            pass: 'custominearmonitor' 
-            /*
-            user: 'iemaniamailer@google.com', 
-            pass: 'custominearmonitor' 
-        host: 'smtp.mail.yahoo.com'
-            */
+            user: 'caga.dbms@gmail.com', 
+            pass: 'caga.dbms1819' 
+          
         }
     });
 
     let mailOptions = {
-        from: '"IEMania Mailer" <iemaniamailer@yahoo.com>',
+        from: '"" <>',
         to: 'tedcapco@gmail.com, garcianashpatrick@gmail.com',
         subject: 'ShoopShop Order',
         //text: req.body.name,
