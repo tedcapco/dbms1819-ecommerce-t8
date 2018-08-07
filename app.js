4b75662f765335438f5ede5d0f9fe065dc00481a
unknown <tedcapco@gmail.com>
2018-08-07 17:13:46 +0800

    update

--- app.js
+++ app.js
@@ -1,95 +1,149 @@
 const express = require('express');
 const path = require('path');
-const exphbs = require('express-handlebars');
 const { Client } = require('pg');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const PORT = process.env.PORT || 5000
 
-// instantiate client using your DB configurations
 const client = new Client({
 	database: 'de067mu7313q4f',
 	user: 'jadjumlpkcstgp',
 	password: 'e253d1d1666a44fbc5ed73da34d52fbf1e4a531f25fbef771a8675104be12168',
 	host: 'ec2-54-204-23-228.compute-1.amazonaws.com',
+	port: 5432,
+	ssl: true
+});
+/*
+const client = new Client({
+	database: 'storedb',
+	user: 'postgres',
+	password: 'admin',
+	host: 'localhost',
 	port: 5432
 });
-
-/*connect to database
+*/
 client.connect()
 	.then(function() {
-		console.log('connected to database!')
+		console.log('connected to database!');
 	})
-	.catch(function(err) {
-		console.log('cannot connect to database!')
-	}); */
+	.catch(function() {
+		console.log('Error');
+	})
 
+/*
+INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Double Monk Strap - Black', 'Footware', 'Originally, The monk strap had a difficult time being placed in either the formal or casual ends of the shoe-wearing spectrum. Considered too casual to be worn with a suit and too formal for many casual environments.', 'Marquins', 2195.00, '/Double Monk Strap Black.jpg');
+INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Longwing - Cognac Brown', 'Footware', 'Greenwich Vintage’s built-to-order cognac brown longwing with white mid-sole and baby blue commando sole.', 'Marquins', 2395.00, '/Longwing Cognac Brown.jpg');
+INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Captoe - Black', 'Footware', 'In the recent past, we covered various types of men’s boots and shoes, both formal and casual. In our Brogues Shoe Guide for Men, we touched lightly upon the difference between Oxford and Derby shoes.', 'Marquins', 2195.00, '/Captoe Black.jpg');
+INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Captoe - Cognac Brown', 'Footware', 'Cap toe Italian Leather dress shoes in contrasting brown leathers. The Cap Toe Griffin Brown Antique is crafted from smooth leather with a cap toe.', 'Marquins', 2195.00, '/Captoe Cognac Brown.jpg');
+INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Brogue Captoe Oak Brown', 'Footware', 'The Half Brogue Kendrik cap toe dress shoes in brown antique is hand-made from premium Italian leather with a cap toe and bold color combination.', 'Marquins', 2195.00, '/Brogue Captoe Oak Brown.jpg');
+INSERT INTO Products01(name, type, description, brand, price, pic) VALUES('Longwing - Black', 'Footware', 'Black French Calf upper framed with matching black edge stain on our Leo last. Historically, a pattern color combination that is recognized as the "work horse" classic business shoe.', 'Marquins', 2395.00, '/Longwing Black.jpg');
+
+*/
+//View engine setup
 const app = express();
-// tell express which folder is a static/public folder
-app.set('views', path.join(__dirname,'views'));
-// tell express to use handlebars as template engine
-app.engine('handlebars', exphbs({defaultLayout:'main'}));
-app.set('view engine','handlebars');
-app.set('port',(process.env.PORT || 3000));
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+//Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-
-app.get('/', function(req, res) {
-	res.send('Hello!!');
-});
-
-/*app.get('/about', function(req, res) {
-	res.send('<h1>About Page</h1>');
-});
-
-app.get('/api/products', function(req, res) {
-
-	client.query('SELECT * FROM Products', (req, data)=>{
-		console.log(data.rows);
-		res.json({
-			data: data.rows
-		})
-	})
-}); 
-app.get('/user/:userName', function(req, res) {
-	const userName = req.params.userName;
-	res.send('<h1>Hi,' + userName + '!!</h1>');
-}); 
 
 app.get('/', function(req,res) {
-	res.render('home',{
-		content: 'This is a sample of a template engine (handlebars)!',  https://www.blogcdn.com/www.joystiq.com/media/2008/01/oasismessin44.jpg
+	client.query('SELECT * FROM Products01', (req, data)=>{
+		var list = [];
+		for (var i = 0; i < data.rows.length; i++) {
+			list.push(data.rows[i]);
+		}
+		res.render('home',{
+			data: list,
+			title: 'Product List'
+		});
 	});
-}); */
+});
 
-app.get('/member/1', function (req, res) {
-	res.render('member', {
-		name: 'Ted Christoffer M. Capco',
-		email: 'tedcapco@gmail.com',
-		phone: '09475768717',
-		imageUrl: '/img/member1.png',
-		hobbies: ['Playing guitar','listening to worship music', 'watching MTVs'],
-		skills: ['Web Development', 'Front End Design'],
-		fb: 'https://www.facebook.com/',
-		tw: '',
-		gmail: 'tedcapco@gmail.com'
-	})
-}); 
+app.get('/products/:id', (req,res)=>{
+	var id = req.params.id;
+	client.query('SELECT * FROM Products01', (req, data)=>{
+		var list = [];
+		for (var i = 0; i < data.rows.length+1; i++) {
+			if (i==id) {
+				list.push(data.rows[i-1]);
+			}
+		}
+		res.render('products',{
+			data: list
+		});
+	});
+});
 
-app.get('/member/2', function (req, res) {
-	res.render('member', {
-		name: 'Patrick Nash de Guzman Garcia',
-		email: 'garcianashpatrick@gmail.com',
-		phone: '09162209550',
-		imageUrl: '/img/member2.png', // http://images5.fanpop.com/image/photos/31300000/Patrick-Stump-patrick-stump-31355378-500-331.jpg
-		hobbies: ['writing poems','essays', 'short stories', 'reading books', 'watercolor painting'],
-		skills: ['Web Dev', 'Leadership'],	
-		fb: 'https://www.facebook.com/patricknash.deguzmangarcia',
-		tw: 'https://l.messenger.com/l.php?u=https%3A%2F%2Ftwitter.com%2Fpagatrigeek&h=AT1SE-xr74Rmvh0XP917YS1IJFoaknfWacMXx2BigjwvP5XIcaXv3o9C6JB0qY6NBs0ZA9SY55BOzR8KZNQwR4prAGjBDYPu7MBO1HiHOcOAEhQz1de3QbLzzdEL_zwFLjtr',
-		gmail: 'garcianashpatrick@gmail.com'
-	})
-}); 
+app.get('/edit', (req,res)=>{
+	var id = req.params.id;
+	res.render('edit');
+});
 
+app.post('/products/:id/send', function(req, res) {
+	console.log(req.body);
+	var id = req.params.id;
+	const output = `
+		<p>You have a new contact request</p>
+		<h3>Contact Details</h3>
+		<ul>
+			<li>Customer Name: ${req.body.name}</li>
+			<li>Phone: ${req.body.phone}</li>
+			<li>Email: ${req.body.email}</li>
+			<li>Product ID: ${req.body.productid}</li>
+			<li>Quantity: ${req.body.quantity}</li>
+		</ul>
+	`;
 
-app.listen(app.get('port'), function() {
+	//nodemailer
+	let transporter = nodemailer.createTransport({
+        host: 'smtp.mail.yahoo.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'iemaniamailer@yahoo.com', 
+            pass: 'custominearmonitor' 
+            /*
+            user: 'iemaniamailer@google.com', 
+            pass: 'custominearmonitor' 
+        host: 'smtp.mail.yahoo.com'
+            */
+        }
+    });
+
+    let mailOptions = {
+        from: '"IEMania Mailer" <iemaniamailer@yahoo.com>',
+        to: 'tedcapco@gmail.com, garcianashpatrick@gmail.com',
+        subject: 'ShoopShop Order',
+        //text: req.body.name,
+        html: output
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        client.query('SELECT * FROM Products01', (req, data)=>{
+			var list = [];
+			for (var i = 0; i < data.rows.length+1; i++) {
+				if (i==id) {
+					list.push(data.rows[i-1]);
+				}
+			}
+			res.render('products',{
+				data: list,
+				msg: '---Email has been sent---'
+			});
+		});
+     });
+});
+
+app.listen(3000,function() {
 	console.log('Server started at port 3000');
-
-}); 
+});
